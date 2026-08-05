91b1e525e30014e29167cd33feb7dc78f9703321
Po'o Technologies <pootechnologies1@gmail.com>
2026-08-05 13:39:43 +0300

    Add Waiter model with name and contact info fields

--- inventory/models.py
+++ inventory/models.py
@@ -54,6 +54,15 @@ class Supplier(models.Model):
     def __str__(self):
         return self.name
 
+class Waiter(models.Model):
+    name = models.CharField(max_length=200, blank=True, null=True)
+    contact_info = models.CharField(max_length=50, null=True, blank=True)
+    user = models.CharField(max_length=255, default="User", null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=200, blank=False, null=False)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
