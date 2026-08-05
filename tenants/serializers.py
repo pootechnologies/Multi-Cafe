55fa65ce62fbdf42d233f2e5aad8f3526f3d0608
Po'o Technologies <pootechnologies1@gmail.com>
2026-08-05 15:52:29 +0300

    Add new fields to chapa serializer

--- tenants/serializers.py
+++ tenants/serializers.py
@@ -26,6 +26,11 @@ class ChapaInitSerializer(serializers.Serializer):
     subscriptionPlan = SubscriptionPlanSerializer(source='plan', read_only=True)
     provider = serializers.CharField(
         read_only=True)
+    reference = serializers.CharField(read_only=True)
+    status = serializers.CharField(read_only=True)
+    payment_url = serializers.URLField(read_only=True, help_text="URL to redirect the user for payment if applicable")
+    paid_at = serializers.DateTimeField(read_only=True)
+    expires_at = serializers.DateField(read_only=True)
 
 class ChapaVerifySerializer(serializers.Serializer):
     # reference = serializers.CharField(required=True)
